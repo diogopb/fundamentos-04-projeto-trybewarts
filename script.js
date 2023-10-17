e33d40164e422cac07253e53d1439257ce5f4164
Diogo Pinto <dpintobarbosa@gmail.com>
2023-10-17 10:24:10 -0300

    resolve requisitos 3 e 4

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('login').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+
+  if (email === 'tryber@teste.com' && password === '123456') {
+    window.alert('Olá, Tryber!');
+  } else {
+    window.alert('Email ou senha inválidos.');
+  }
+});
